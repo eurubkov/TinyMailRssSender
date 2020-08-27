09630d6f07057c2495c0dc1bb31b66ca106ca1c9
= <=>
2020-08-27 19:33:33 -0400

    rename local_file_name to blob_name

--- CheckRssFeedFunction/storageservice.py
+++ CheckRssFeedFunction/storageservice.py
@@ -4,16 +4,16 @@ from datetime import datetime
 
 azure_storage_conn_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 container_name = "rsshandler"
-local_file_name = "checkpoint.txt"
+blob_name = "checkpoint.txt"
 
 def create_or_update_blob(now):
     blob_service_client = BlobServiceClient.from_connection_string(azure_storage_conn_str)
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
     blob_client.upload_blob(str(now), overwrite=True)
 
 def __get_checkpoint_blob__():
     blob_service_client = BlobServiceClient.from_connection_string(azure_storage_conn_str)
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
     download_stream = blob_client.download_blob()
     checkpointbytes = download_stream.readall()
     return str(checkpointbytes, 'utf-8')
