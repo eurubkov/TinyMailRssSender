e5e543dd6d1608d056943ab3758586818afdabcc
= <10166308+eurubkov@users.noreply.github.com>
2022-01-20 23:36:58 -0800

    Best code ever

--- CheckRssFeedFunction/storageservice.py
+++ CheckRssFeedFunction/storageservice.py
@@ -1,4 +1,5 @@
-import os, uuid
+import os
+import uuid
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 from datetime import datetime
 
@@ -6,19 +7,27 @@ azure_storage_conn_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 container_name = "rsshandler"
 blob_name = "checkpoint.txt"
 
+
 def create_or_update_blob(now):
-    blob_service_client = BlobServiceClient.from_connection_string(azure_storage_conn_str)
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+    blob_service_client = BlobServiceClient.from_connection_string(
+        azure_storage_conn_str)
+    blob_client = blob_service_client.get_blob_client(
+        container=container_name, blob=blob_name)
     blob_client.upload_blob(str(now), overwrite=True)
 
+
 def __get_checkpoint_blob__():
-    blob_service_client = BlobServiceClient.from_connection_string(azure_storage_conn_str)
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+    blob_service_client = BlobServiceClient.from_connection_string(
+        azure_storage_conn_str)
+    blob_client = blob_service_client.get_blob_client(
+        container=container_name, blob=blob_name)
     download_stream = blob_client.download_blob()
     checkpointbytes = download_stream.readall()
     return str(checkpointbytes, 'utf-8')
 
+
 def get_checkpoint_datetime():
     checkpoint_datetime = datetime.utcnow
-    checkpoint_datetime = datetime.strptime(__get_checkpoint_blob__(), '%Y-%m-%d %H:%M:%S.%f')
+    checkpoint_datetime = datetime.strptime(
+        __get_checkpoint_blob__(), '%Y-%m-%d %H:%M:%S.%f')
     return checkpoint_datetime
